a5837192f95a6c1f070c058a4684f8525b82d856
LukeZuellRMIT <100609087+LukeZuellRMIT@users.noreply.github.com>
2023-04-13 23:49:37 +1000

    tomorrows game

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function createTable(data) {
 }
 
 async function displayData() {
-    const data = await fetchData("https://cors-anywhere.herokuapp.com/https://api.afl.com.au/cfs/afl/playerStats/match/CD_M20230140401");
+    const data = await fetchData("https://cors-anywhere.herokuapp.com/https://api.afl.com.au/cfs/afl/matchInterchange/CD_M20230140502");
     const processedHomeData = processData(data, "Home");
     const processedAwayData = processData(data, "Away");
     //console.log(processedData);
