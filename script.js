b65cb78fdaf43f50072ec989be1b07f466839b8c
LukeZuellRMIT <100609087+LukeZuellRMIT@users.noreply.github.com>
2023-04-14 02:23:01 +1000

    trying new fetch

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-async function fetchData(url) {
+async function fetchData() {
     try {
-        const response = await fetch(url, {
-            "credentials": "omit",
-            "headers": {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0",
-                "Accept": "*/*",
-                "Accept-Language": "en-US,en;q=0.5",
-                "x-media-mis-token": "f98212b7dff7ab75640bab2acede3323",
-                "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-site",
-                "Sec-GPC": "1"
-            },
-            "referrer": "https://www.afl.com.au/",
-            "method": "GET",
-            "mode": "cors"
-        });
-        const responseText = await response.text();
-        //console.log('Raw response text:', responseText);
-        const data = JSON.parse(responseText);
+        const response = await fetch('/api/fetch-data');
+        const data = await response.json();
         return data;
     } catch (error) {
-        console.log('Not working:', error);
         console.error('Error fetching data:', error);
     }
 }
